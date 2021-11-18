a74699d6ba0d9582976a7a4198c91e8737f4d88c
nguyenvanty <ty.nguyenvan@hdwebsoft.co>
2021-11-18 11:45:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 function App() {
+  const [backgroundBtn, setBackgroundBtn] = useState("red");
+  const [disableBtn, setDisableBtn] = useState(false);
+  const handleChangeColor = () => {
+    if (backgroundBtn === "red") {
+      setBackgroundBtn("blue");
+    } else {
+      setBackgroundBtn("red");
+    }
+  };
+  const handleCheckbox = (e) => {
+    const { checked } = e.target;
+    if (checked) {
+      setDisableBtn(true);
+      setBackgroundBtn("gray");
+    } else {
+      setDisableBtn(false);
+      setBackgroundBtn("blue");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <button
+        disabled={disableBtn}
+        onClick={handleChangeColor}
+        style={{ backgroundColor: `${backgroundBtn}` }}
+      >
+        {backgroundBtn === "red" ? "Change to blue" : "Change to red"}
+      </button>
+      <input
+        checked={disableBtn}
+        id="disable-button-checkbox"
+        onChange={(e) => handleCheckbox(e)}
+        type="checkbox"
+      />
+        <label htmlFor="disable-button-checkbox">{`Disable button "Change to blue"`}</label>
+ 
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,75 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { replaceCamelWithSpaces } from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // trong component App có tồn tại text "learn react" hay không
+  // const linkElement = screen.getByText("Learn React");
+  // kiểm tra các thẻ link (a) có text nào tên là "learn react" hay không
+  const linkElement = screen.getByRole("link", { name: /learn react/i });
+  // hiển thị kết quả test (thành công - thất bại)
   expect(linkElement).toBeInTheDocument();
+
+  // throw new Error("test failed")
 });
+test("button has correct initial color", () => {
+  render(<App />);
+  // btn này có text ban đầu là "Change to blue"
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  // backgroundColor ban đầu của btn này là "red"
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  // khi click btn
+  fireEvent.click(colorButton);
+  // thì màu của btn sẽ đổi thành "blue"
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  // và cái text của btn sẽ đổi thành "Change to red"
+  expect(colorButton.textContent).toBe("Change to red");
+});
+test("initial conditions", () => {
+  render(<App />);
+  // có nhiều hơn 1 btn có text là: "Change to blue"
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  // btn này không bị disable (đang hoạt động)
+  expect(colorButton).toBeEnabled();
+  // có nhiều hơn 1 checkbox
+  const checkbox = screen.getByRole("checkbox");
+  // checkbox này chưa được check (tích chọn)
+  expect(checkbox).not.toBeChecked();
+});
+test("checkbox disable button on first click and enable on second click", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox", {name: `Disable button "Change to blue"`});
+  const button = screen.getByRole("button", {name: "Change to blue"});
+  // khi click vào checkbox lần đầu tiên
+  fireEvent.click(checkbox);
+  // thì cái btn này sẽ bị disable
+  expect(button).toBeDisabled();
+  // khi click vào checkbox lần thứ 2
+  fireEvent.click(checkbox);
+  // thì cái btn này sẽ enable trở lại
+  expect(button).toBeEnabled();
+});
+test("Clicked disabled button has gray background and reverts to blue", () => {
+  render(<App/>);
+  const checkbox = screen.getByRole("checkbox", {name: `Disable button "Change to blue"`});
+  const button = screen.getByRole("button", {name: "Change to blue"})
+  fireEvent.click(button);
+  fireEvent.click(checkbox)
+  expect(button).toHaveStyle("background-color:gray");
+  fireEvent.click(checkbox)
+  expect(button).toHaveStyle("background-color:blue");
+})
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    // tobe -> đổi thành
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+  test("Works for one inner capital letters", () => {
+    // tobe -> đổi thành
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Works for multiple inner capital letters", () => {
+    // tobe -> đổi thành
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
+})
